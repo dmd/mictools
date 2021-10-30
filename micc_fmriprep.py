a906e7b38998eefa00fe6463bfdfbbf86ec16851
Daniel M. Drucker <ddrucker@mclean.harvard.edu>
2021-10-30 17:53:15 -0400

    fmri version bump

--- micc_fmriprep.py
+++ micc_fmriprep.py
@@ -240,8 +240,8 @@ if __name__ == "__main__":
 
     versioning.add_argument(
         "--fmriprep-version",
-        help="fmriprep version number. Default: 20.2.5",
-        default="20.2.5",
+        help="fmriprep version number. Default: 20.2.6",
+        default="20.2.6",
     )
 
     args = parser.parse_args()
