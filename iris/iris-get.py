994e13d363bf5fad839fec260401a1a6ba386fb3
Daniel M. Drucker <ddrucker@mclean.harvard.edu>
2021-12-03 16:16:58 -0500

    allow flat output

--- iris/iris-get.py
+++ iris/iris-get.py
@@ -15,15 +15,39 @@ parser.add_argument(
     required=False,
     default=getpass.getuser(),
 )
+
+parser.add_argument(
+    "-f",
+    "--flat",
+    help="Store all dicom files in one folder - not structured by scan.",
+    required=False,
+    action="store_true",
+)
+
 parser.add_argument("accessionnumber", nargs="+")
 args = parser.parse_args()
 
 password = getpass.getpass(f"Iris password for {args.username}: ")
 
+dlformat = "native"
+outdir = "."
+
+
 for accessionnumber in args.accessionnumber:
+    if args.flat:
+        dlformat = "flat"
+        outdir = accessionnumber
     print(f"Fetching {accessionnumber}...")
     subprocess.call(
-        ["/cm/shared/anaconda3/envs/iris/bin/ArcGet.py", "-f", "native", "-l", accessionnumber],
+        [
+            "/cm/shared/anaconda3/envs/iris/bin/ArcGet.py",
+            "-f",
+            dlformat,
+            "-l",
+            accessionnumber,
+            "-o",
+            outdir,
+        ],
         env=dict(
             os.environ,
             XNAT_PASS=password,
