99d9de61071c659c2acd61c7864afb0478b4a88f
Daniel M. Drucker <ddrucker@mclean.harvard.edu>
2022-08-15 12:45:43 -0400

    new ME test attempt

--- iris/test_converters.py
+++ iris/test_converters.py
@@ -11,59 +11,40 @@ testdir_orig = testdir + ".orig"
 
 
 def test_final_scan():
-    assert final_scan(
-        ["FOO_BAR_11.nii.gz", "FOO_BAR_2.nii.gz", "FOO_BAR_3.nii.gz"], False
-    ) == ["FOO_BAR_11.nii.gz"]
+    assert final_scan(["foo_11.nii.gz", "foo_2.nii.gz", "foo_3.nii.gz"], False) == [
+        "foo_11.nii.gz"
+    ]
 
     assert final_scan(
         [
-            "checkerboard_AP_SBRef_37_e1.nii.gz",
-            "checkerboard_AP_SBRef_37_e2.nii.gz",
-            "checkerboard_AP_SBRef_37_e3.nii.gz",
-            "checkerboard_AP_SBRef_38_e1.nii.gz",
-            "checkerboard_AP_SBRef_38_e2.nii.gz",
-            "checkerboard_AP_SBRef_38_e3.nii.gz",
+            "foo_37_e1.nii.gz",
+            "foo_37_e2.nii.gz",
+            "foo_38_e1.nii.gz",
+            "foo_38_e2.nii.gz",
         ],
         True,
     ) == [
-        "checkerboard_AP_SBRef_38_e1.nii.gz",
-        "checkerboard_AP_SBRef_38_e2.nii.gz",
-        "checkerboard_AP_SBRef_38_e3.nii.gz",
+        "foo_38_e1.nii.gz",
+        "foo_38_e2.nii.gz",
     ]
 
 
 def test_convert_to_bids():
     shutil.copytree(testdir, testdir_orig, copy_function=os.link)
 
-    convert_to_nifti(f"{testdir}/EXAMPLE")
+    convert_to_nifti(f"{testdir}/phantom-20220812")
 
     should_exist_nifti = [
-        "T1_MEMPRAGE_64ch_6_e1.json",
-        "T1_MEMPRAGE_64ch_6_e1.nii.gz",
-        "T1_MEMPRAGE_64ch_6_e2.json",
-        "T1_MEMPRAGE_64ch_6_e2.nii.gz",
-        "T1_MEMPRAGE_64ch_6_e3.json",
-        "T1_MEMPRAGE_64ch_6_e3.nii.gz",
-        "T1_MEMPRAGE_64ch_6_e4.json",
-        "T1_MEMPRAGE_64ch_6_e4.nii.gz",
-        "T1_MEMPRAGE_64ch_RMS_7.json",
-        "T1_MEMPRAGE_64ch_RMS_7.nii.gz",
-        "restingstate_10.json",
-        "restingstate_10.nii.gz",
-        "restingstate_11_ph.json",
-        "restingstate_11_ph.nii.gz",
-        "checkerboard_AP_SBRef_37_e1.json",
-        "checkerboard_AP_SBRef_37_e1.nii.gz",
-        "checkerboard_AP_SBRef_37_e2.json",
-        "checkerboard_AP_SBRef_37_e2.nii.gz",
-        "checkerboard_AP_SBRef_37_e3.json",
-        "checkerboard_AP_SBRef_37_e3.nii.gz",
-        "checkerboard_AP_SBRef_38_e1.json",
-        "checkerboard_AP_SBRef_38_e1.nii.gz",
-        "checkerboard_AP_SBRef_38_e2.json",
-        "checkerboard_AP_SBRef_38_e2.nii.gz",
-        "checkerboard_AP_SBRef_38_e3.json",
-        "checkerboard_AP_SBRef_38_e3.nii.gz",
+        "T1w_MEMPRAGE_RMS_7.nii.gz",
+        "T1w_MEMPRAGE_RMS_9.nii.gz",
+        "T1w_MEMPRAGE_RMS_11.nii.gz",
+        "T1w_MEMPRAGE_10_e2.nii.gz",  # ensure updated dcm2niix that names echoes correctly
+        "rfMRI_REST_SINGLEECHO_AP_35.nii.gz",
+        "rfMRI_REST_SINGLEECHO_AP_37.nii.gz",
+        "rfMRI_REST_SINGLEECHO_AP_39.nii.gz",
+        "rfMRI_REST_SINGLEECHO_PA_33.nii.gz",
+        "rfMRI_REST_MULTIECHO_AP_19_e4.nii.gz",
+        "rfMRI_REST_MULTIECHO_PA_21_e4.nii.gz",
     ]
 
     should_exist_bids = [
@@ -73,24 +54,28 @@ def test_convert_to_bids():
         "dataset_description.json",
         "sub-test/ses-1/anat/sub-test_ses-1_T1w.json",
         "sub-test/ses-1/anat/sub-test_ses-1_T1w.nii.gz",
-        "sub-test/ses-1/func/sub-test_ses-1_task-resting_bold.nii.gz",
-        "sub-test/ses-1/func/sub-test_ses-1_task-resting_bold.json",
-        "sub-test/ses-1/func/sub-test_ses-1_task-checkerboard_dir-AP_echo-1_bold.nii.gz",
-        "sub-test/ses-1/func/sub-test_ses-1_task-checkerboard_dir-AP_echo-1_bold.json",
-        "sub-test/ses-1/func/sub-test_ses-1_task-checkerboard_dir-AP_echo-2_bold.nii.gz",
-        "sub-test/ses-1/func/sub-test_ses-1_task-checkerboard_dir-AP_echo-2_bold.json",
-        "sub-test/ses-1/func/sub-test_ses-1_task-checkerboard_dir-AP_echo-3_bold.nii.gz",
-        "sub-test/ses-1/func/sub-test_ses-1_task-checkerboard_dir-AP_echo-3_bold.json",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-singleecho_dir-AP_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-singleecho_dir-AP_bold.json",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-singleecho_dir-PA_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-singleecho_dir-PA_bold.json",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-AP_run-1_echo-1_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-AP_run-1_echo-2_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-AP_run-1_echo-3_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-AP_run-1_echo-4_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-PA_run-1_echo-1_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-PA_run-1_echo-2_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-PA_run-1_echo-3_bold.nii.gz",
+        "sub-test/ses-1/func/sub-test_ses-1_task-rest_acq-multiecho_dir-PA_run-1_echo-4_bold.nii.gz",
     ]
 
     for f in should_exist_nifti:
-        assert Path(f"{testdir}/EXAMPLE/nifti/{f}").exists()
+        assert Path(f"{testdir}/phantom-20220812/nifti/{f}").exists()
 
-    config = yaml.safe_load(open(f"{testdir}/EXAMPLE.yaml"))
-    convert_to_bids(config, f"{testdir}/EXAMPLE", "test", "1")
+    config = yaml.safe_load(open(f"{testdir}/phantom-20220812.yaml"))
+    convert_to_bids(config, f"{testdir}/phantom-20220812", "test", "1")
 
     for f in should_exist_bids:
-        assert Path(f"{testdir}/EXAMPLE/{f}").exists()
+        assert Path(f"{testdir}/phantom-20220812/{f}").exists()
 
     # clean up
     shutil.rmtree(testdir)
