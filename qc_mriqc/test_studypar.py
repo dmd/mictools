9aabafdb6ea9864d9937838d237a2344de94d093
Daniel Druckr <ddrucker@mclean.harvard.edu>
2022-08-17 17:10:21 -0400

    rename method

--- qc_mriqc/test_studypar.py
+++ qc_mriqc/test_studypar.py
@@ -6,14 +6,14 @@ yaml = "qc_mriqc/qctypes.yaml"
 
 def test_studypar_files():
     s = studypar.Studypar(testdir + "/studypar.Bergman.ss25m.THC-fMRI", yaml)
-    m = s.matches()
+    m = s.info()
     assert m
     assert m["emails"] == ["ddrucker@mclean.harvard.edu"]
     assert m["folder"] == "THC"
     assert m["ident"] == "ss25m"
 
     s = studypar.Studypar(testdir + "/studypar.Kohut.ss15f.CathinoneWinj", yaml)
-    m = s.matches()
+    m = s.info()
     assert m
     assert m["emails"] == ["ddrucker@mclean.harvard.edu"]
     assert m["folder"] == "cathinone"
