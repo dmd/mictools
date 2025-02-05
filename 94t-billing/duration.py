adff9457fee5c525e557bc15ccfc70af0c313784
Daniel M. Drucker <dmd@3e.org>
2025-02-05 17:45:48 -0500

    by date too

--- 94t-billing/duration.py
+++ 94t-billing/duration.py
@@ -15,6 +15,19 @@ username, _, password = netrc.netrc("netrc").authenticators(host)
 o = pyorthanc.Orthanc(server, username=username, password=password)
 
 
+def get_date():
+    if len(sys.argv) > 1 and len(sys.argv[1]) == 8 and sys.argv[1].isdigit():
+        return sys.argv[1]
+    else:
+        return datetime.datetime.today().strftime("%Y%m%d")
+
+
+def studies_for_date(study_date):
+    query = {"StudyDate": study_date}
+    studies = pyorthanc.find_studies(client=o, query=query)
+    return studies
+
+
 def duration(study):
     # use bulk-content as it's vastly faster than the pyorthanc method
 
@@ -40,38 +53,40 @@ def duration(study):
 
     return max(instance_creation_datetimes) - min(instance_creation_datetimes)
 
-def studies_for_date(study_date):
-    query = {"StudyDate": study_date}
-    studies = pyorthanc.find_studies(client=o, query=query)
-    return studies
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: %s <accession number>" % sys.argv[0])
-        sys.exit(1)
-
-
-    accnum = sys.argv[1]
-    query = {"AccessionNumber": accnum}
-    studies = pyorthanc.find_studies(client=o, query=query)
-    study = studies[0]
-
-    if len(studies) == 0:
-        print(f"No studies found with query {repr(query)}")
-        sys.exit(1)
+def get_study(study_id):
+    if type(study_id) == str and study_id.startswith("E"):
+        accnum = study_id
+        query = {"AccessionNumber": accnum}
+        studies = pyorthanc.find_studies(client=o, query=query)
+        if len(studies) == 0:
+            print(f"No studies found with query {repr(query)}")
+            sys.exit(1)
+        study = studies[0]
+    else:
+        study = study_id
 
     data = {}
     data["date"] = study.date
     data["AccessionNumber"] = study.main_dicom_tags["AccessionNumber"]
-    if study.main_dicom_tags["StudyDescription"].count("^") > 1:
-        data["investigator"] = study.main_dicom_tags["StudyDescription"].split("^")[1]
+    sd = study.main_dicom_tags["StudyDescription"]
+    if sd.startswith("Investigators^") or sd.startswith("McLean Research^"):
+        data["investigator"] = sd.split("^")[1]
     else:
-        data["investigator"] = ""
-    data["protocol"] = study.main_dicom_tags["StudyDescription"]
+        data["investigator"] = "clinical"
+    data["protocol"] = sd
     data["duration"] = str(duration(study)).split(".")[0]
 
     writer = csv.writer(sys.stdout)
     writer.writerow(data.values())
+
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1].startswith("E"):
+        get_study(sys.argv[1])
+    else:
+        for study in studies_for_date(get_date()):
+            get_study(study)
 
 
 if __name__ == "__main__":
